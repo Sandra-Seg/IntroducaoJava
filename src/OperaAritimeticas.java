52a04412c3b3653d424232c92e4fe803f8749f36
1352518483 <1352518483@ulife.com.br>
2025-04-22 10:53:43 -0300

    asd

---
+++ src/OperaAritimeticas.java
@@ -0,0 +1,21 @@
+public class OperaAritimeticas {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+
+
+        int subtracao = numero1 - numero2;
+        int soma = numero1 + numero2;
+        int divisao = numero1 / numero2;
+        int produto = numero1 * numero2;
+
+        System.out.println("A subtracao de " + numero1 + " - " + numero2 + " é: " + subtracao);
+        System.out.println("A soma de " + numero1 + " + " + numero2 + " é: " + soma);
+        System.out.println("A divisao de " + numero1 + " / " + numero2 + " é: " + divisao);
+        System.out.println("o produto de " + numero1 + " * " + numero2 + " é: " + produto);
+
+
+    }
+
+}
+
